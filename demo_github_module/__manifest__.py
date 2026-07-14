efdac79909a08c3ebac39eb9a359d1d01e65afe8
ykashyap <ykashyap@surekhatech.com>
2026-07-14 19:28:08 +0530

    Add my changes

---
+++ demo_github_module/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Demo GitHub Module',
+    'version': '1.0',
+    'summary': 'A demo module to test GitHub pushes and automated PR workflows.',
+    'description': """
+Demo GitHub Module
+==================
+This is a lightweight Odoo module designed specifically to test Git version control
+and GitHub Pull Request RAG integrations without exposing proprietary company code.
+    """,
+    'category': 'Hidden/Tests',
+    'author': 'Webseed',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
